90b23c24e4b3fbc8363eec1e969b2bdfffae19ce
Masaki Hara <ackie.h.gmai@gmail.com>
2024-09-19 12:06:44 +0900

    Split string

--- types/langtypes.ts
+++ types/langtypes.ts
@@ -25,6 +25,11 @@ export {
   Number_unsignedRightShift,
   NumberBitwiseOp,
 } from "./langtypes/numeric.ts";
+export {
+  isString,
+  StringIndexOf,
+  StringLastIndexOf,
+} from "./langtypes/string.ts";
 
 /**
  * 6.1 ECMAScript Language Types https://tc39.es/ecma262/multipage/ecmascript-data-types-and-values.html#sec-ecmascript-language-types
@@ -78,52 +83,6 @@ export function isNull(value: unknown): value is null {
 export function isBoolean(value: unknown): value is boolean {
   // Use typeof https://tc39.es/ecma262/multipage/ecmascript-language-expressions.html#sec-typeof-operator-runtime-semantics-evaluation
   return typeof value === "boolean";
-}
-
-/**
- * 6.1.4 The String Type https://tc39.es/ecma262/multipage/ecmascript-data-types-and-values.html#sec-ecmascript-language-types-string-type
- *
- * > The String type is the set of all ordered sequences of zero or more 16-bit unsigned integer values (“elements”) up to a maximum length of 2**53 - 1 elements.
- *
- * @param value value to check
- * @returns true if value is a string
- */
-export function isString(value: unknown): value is string {
-  // Use typeof https://tc39.es/ecma262/multipage/ecmascript-language-expressions.html#sec-typeof-operator-runtime-semantics-evaluation
-  return typeof value === "string";
-}
-
-/**
- * 6.1.4.1 StringIndexOf ( string, searchValue, fromIndex ) https://tc39.es/ecma262/multipage/ecmascript-data-types-and-values.html#sec-stringindexof
- */
-export function StringIndexOf(
-  string: string,
-  searchValue: string,
-  fromIndex: number,
-): number | "NOT-FOUND" {
-  // Use String.prototype.indexOf https://tc39.es/ecma262/multipage/text-processing.html#sec-string.prototype.indexof
-  if (fromIndex > string.length) {
-    // StringIndexOf("a", "", 2) is NOT-FOUND while "a".indexOf("", 2) is 1
-    // Note that, we assume fromIndex to be a nonnegative integer, so we don't check for values outside of this range
-    return "NOT-FOUND";
-  }
-  const idx = string.indexOf(searchValue, fromIndex);
-  return idx === -1 ? "NOT-FOUND" : idx;
-}
-
-/**
- * 6.1.4.2 StringLastIndexOf ( string, searchValue, fromIndex ) https://tc39.es/ecma262/multipage/ecmascript-data-types-and-values.html#sec-stringlastindexof
- */
-export function StringLastIndexOf(
-  string: string,
-  searchValue: string,
-  fromIndex: number,
-): number | "NOT-FOUND" {
-  // Use String.prototype.lastIndexOf https://tc39.es/ecma262/multipage/text-processing.html#sec-string.prototype.lastindexof
-  // There is an Assert check in the spec to ensure fromIndex is already clamped to an appropriate range.
-  // So we do not insert range check here for now.
-  const idx = string.lastIndexOf(searchValue, fromIndex);
-  return idx === -1 ? "NOT-FOUND" : idx;
 }
 
 /**
