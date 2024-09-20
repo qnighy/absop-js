9ae549efeff08bc247176a65568abc3fc3464bde
Masaki Hara <ackie.h.gmai@gmail.com>
2024-09-20 18:22:13 +0900

    Add Set

--- absops/object.ts
+++ absops/object.ts
@@ -4,14 +4,37 @@
 
 import { type LanguageValue, type StrictPropertyKey } from "../types.ts";
 
+/**
+ * 7.3.2 Get ( O, P ) https://tc39.es/ecma262/multipage/abstract-operations.html#sec-get-o-p
+ */
 export function Get(o: object, p: StrictPropertyKey): LanguageValue {
   // deno-lint-ignore no-explicit-any
   return (o as any)[p];
 }
 
+/**
+ * 7.3.3 GetV ( V, P ) https://tc39.es/ecma262/multipage/abstract-operations.html#sec-getv
+ */
 export function GetV(v: LanguageValue, p: StrictPropertyKey): LanguageValue {
   // deno-lint-ignore no-explicit-any
   return (v as any)[p];
+}
+
+/**
+ * 7.3.4 Set ( O, P, V, Throw ) https://tc39.es/ecma262/multipage/abstract-operations.html#sec-set-o-p-v-throw
+ */
+export function Set(
+  o: object,
+  p: StrictPropertyKey,
+  v: LanguageValue,
+  throw_: boolean,
+): void {
+  if (throw_) {
+    // Note that, as we are in a module, this statement runs in strict mode.
+    (o as Record<PropertyKey, unknown>)[p] = v;
+  } else {
+    Reflect.set(o, p, v, o);
+  }
 }
 
 export function GetMethod(
